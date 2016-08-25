38c5ddc57e56b5679faae0bf88e0e196a50d1c0b
ddrocco <keredoccor@gmail.com>
2016-08-24 23:56:27 -0700

    Added robbits

---
+++ Assets/RobotBehavior.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RobotActionCommand {
+	public virtual void Command() {}
+}
+
+public class ForwardCommand : RobotActionCommand {
+	public override void Command() {
+		return;
+	}
+}
+
+public class RobotBehavior : MonoBehaviour {
+
+	List<RobotActionCommand> commands = new List<RobotActionCommand>();
+	int current_command;
+	public float force_const = 50f;
+
+
+	// Use this for initialization
+	void Start () {
+		current_command = 0;
+		commands.Add(new ForwardCommand());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		commands[current_command].Command();
+		this.GetComponent<Rigidbody>().AddForce(force_const * Time.deltaTime * transform.forward);
+	}
+}
