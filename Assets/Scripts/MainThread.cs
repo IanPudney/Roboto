adaaeeb3abdcc5a37b76d2dcb9c9ea538b7ade2d
Ian Pudney <ian.pudney.git@gmail.com>
2016-08-25 21:24:07 -0700

    Adding programming framework

---
+++ Assets/Scripts/MainThread.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.Scripting;
+using System;
+using System.Threading;
+
+public class MainThread : MonoBehaviour
+{
+    public delegate void Func();
+    static Queue<Func> functions = new Queue<Func>();
+    static Queue<Semaphore> sems = new Queue<Semaphore>();
+    Mutex mutex = new Mutex();
+
+    public static MainThread main
+    {
+        get; private set;
+    }
+
+    public void Awake()
+    {
+        main = this;
+    }
+
+    public Semaphore runCoroutine(IEnumerator numerator)
+    {
+        mutex.WaitOne();
+        Func f = (new CoroutineBinder(this, numerator)).execute;
+        functions.Enqueue(f);
+        Semaphore s = new Semaphore(0, 1);
+        sems.Enqueue(s);
+        mutex.ReleaseMutex();
+        return s;
+        // don't wait on the semaphore, because this starts it async
+    }
+
+    public void waitCoroutine(IEnumerator numerator)
+    {
+        runCoroutine(numerator).WaitOne();
+    }
+
+    public void Run(Func f)
+    {
+        mutex.WaitOne();
+        functions.Enqueue(f);
+        Semaphore s = new Semaphore(0, 1);
+        sems.Enqueue(s);
+        mutex.ReleaseMutex();
+        s.WaitOne();
+    }
+
+    void Update()
+    {
+        mutex.WaitOne();
+        while (functions.Count > 0) {
+            Func f = functions.Dequeue();
+            Semaphore s = sems.Dequeue();
+            mutex.ReleaseMutex();
+            f();
+            s.Release();
+            mutex.WaitOne();
+        }
+        mutex.ReleaseMutex();
+    }
+}
+
+class CoroutineBinder
+{
+    IEnumerator numerator;
+    MonoBehaviour behaviour;
+    public CoroutineBinder(MonoBehaviour behaviour, IEnumerator numerator)
+    {
+        this.behaviour = behaviour;
+        this.numerator = numerator;
+    }
+    public void execute()
+    {
+        behaviour.StartCoroutine(numerator);
+    }
+}
---
+++ Assets/Scripts/RobotEditor.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Threading;
+
+public class RobotEditor : MonoBehaviour {
+    public Text input;
+
+    // Use this for initialization
+    void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Submit()
+    {
+        Thread t = new Thread(RunCommand);
+        t.Start();
+    }
+    public void RunCommand()
+    {
+        string cmd = input.text; // todo: fix this race condition (input could change before thread runs)
+        Interpreter python = new Interpreter();
+        string results = "";
+        try
+        {
+            python.Compile("from UnityEngine import *");
+            PyHelper.ImportAllRoot(python);
+            CompileBinder toCall = new CompileBinder(python, "robot = GameObject.Find('Robot').GetComponent[Robot]()\n", Microsoft.Scripting.SourceCodeKind.SingleStatement);
+            MainThread.Func func = toCall.execute;
+            MainThread.main.Run(func);
+            python.Compile(cmd, Microsoft.Scripting.SourceCodeKind.Statements);
+        }
+        catch (System.Exception ex)
+        {
+            results += ex.ToString();
+            throw ex;
+        }
+        finally
+        {
+            results += python.GetOutput();
+            Debug.Log(results);
+        }
+
+    }
+}
+class CompileBinder
+{
+    public Interpreter python;
+    public string source;
+    public Microsoft.Scripting.SourceCodeKind kind;
+    public CompileBinder(Interpreter python, string source, Microsoft.Scripting.SourceCodeKind kind)
+    {
+        this.python = python;
+        this.source = source;
+        this.kind = kind;
+    }
+    public void execute()
+    {
+        python.Compile(source, kind);
+    }
+}
